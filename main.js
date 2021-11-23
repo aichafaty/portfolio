65aa74611b941d8fc93b566b7070ffc81bac84a0
aichafaty <aichafatybittyba@gmail.com>
2021-11-23 19:53:57 +0100

    rendu mobile first non fini pas de css ni rien

---
+++ main.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+    back_to_top = document.getElementById("btn-back-to-top");
+    window.onscroll = function() {scrollFunction()};
+    
+    function scrollFunction() {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        back_to_top.style.display = "block"
+      } else {
+        back_to_top.display = "none";
+      }
+    };
